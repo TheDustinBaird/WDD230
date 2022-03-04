2f2a6d136a4b3273fa6431ab3144b086452dc68a
TheDustinBaird <dbaird99@gmail.com>
2022-03-04 10:34:08 -0700

    9-10

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,11 +1,12 @@
 const requestURL = 'https://byui-cit230.github.io/canvas-referenced/latter-day-prophets.json';
+
 fetch(requestURL)
   .then(function(response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);
     const prophets = jsonObject['prophets'];
+    //console.table(jsonObject);
     prophets.forEach(displayProphets);
   });
 
@@ -15,13 +16,25 @@ fetch(requestURL)
       let portrait = document.createElement('img');
       let dob = document.createElement('p');
       let bplace = document.createElement('p');
-
-  
+      let ordinal = ``;
       let fullname = `${prophet.name} ${prophet.lastname}`;
+
+      
+    if (prophet.order === 1) {
+      ordinal = `${prophet.order}st`;
+    } else if (prophet.order === 2) {
+      ordinal = `${prophet.order}nd`;
+    } else if (prophet.order === 3) {
+      ordinal = `${prophet.order}rd`;
+    } else  {
+      ordinal = `${prophet.order}th`;
+    } 
+
 
       h2.textContent = fullname;
       portrait.setAttribute('src',prophet.imageurl);
-      portrait.setAttribute('alt',fullname, prophet.order);
+      portrait.setAttribute('alt',"Portrait of " + fullname +" - " + ordinal + " Latter-day President");
+      portrait.setAttribute('loading', 'lazy');
       dob.textContent = "Date of Birth: " + `${prophet.birthdate}`;
       bplace.textContent = "Place of Birth: " + `${prophet.birthplace}`;
 
@@ -31,5 +44,5 @@ fetch(requestURL)
       card.appendChild(bplace);
       card.appendChild(portrait);
 
-      document.querySelector('.cards').appendChild(card);
+      document.querySelector('div.cards').appendChild(card);
      }
