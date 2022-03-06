29037e199ef3eda569a65f0eecfd1cd0ea4b6c06
TheDustinBaird <dbaird99@gmail.com>
2022-03-06 02:20:43 -0700

    10-1

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,8 +1,8 @@
-// const requestURL = 'https://macewhit.github.io/wdd230/chamber/data.json';
+
 const requestURL = "data.json"
 const cards = document.querySelector('div.cards');
-const listButton = document.querySelector("#list-btn");
-const cardButton = document.querySelector("#card-btn");
+const listButton = document.querySelector("#listbtn");
+const cardButton = document.querySelector("#cardbtn");
 
 
 fetch(requestURL)
@@ -11,35 +11,34 @@ fetch(requestURL)
     })
     .then(function (jsonObject) {
         const businesses = jsonObject['businesses'];
-        // console.table(jsonObject);
         businesses.forEach(displayBusinesses);
     });
 
 function displayBusinesses(business) {
   let card = document.createElement('section');
 
-  //img
+
   let image = document.createElement('img');
   image.src = business.imgfile;
   image.setAttribute('alt', business.name);
   card.appendChild(image);
 
-  //h2 name
+  
   let h2 = document.createElement('h2');
   h2.textContent = business.name;
   card.appendChild(h2);
 
-  //p address
+
   let p1 = document.createElement('p');
   p1.textContent = business.address;
   card.appendChild(p1);
 
-  //p phone
+
   let p2 = document.createElement('p');
   p2.textContent = business.phone;
   card.appendChild(p2);
 
-  //p website
+  
   let p3 = document.createElement('p');
   p3.textContent = business.website;
   card.appendChild(p3);
