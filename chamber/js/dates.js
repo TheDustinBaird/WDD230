0d8db402591e2260a20ef70410060eb3b40d7eaa
TheDustinBaird <dbaird99@gmail.com>
2022-03-22 08:10:57 -0600

    11-14

--- chamber/js/dates.js
+++ chamber/js/dates.js
@@ -5,9 +5,11 @@ const lastupdated = document.querySelector('#lastupdated');
 const d = new Date();
 const year = d.getFullYear();
 
-if(d.getDay() === 1 || d.getDay() === 2) {
+if(d.getDay() === 2 || d.getDay() === 1) {
 	const banner = document.querySelector("#banner");
+    banner.style.display = "block";
 }
+console.log()
 
 const copyrightyear = document.querySelector('#copyrightyear');
 copyrightyear.textContent = year;
